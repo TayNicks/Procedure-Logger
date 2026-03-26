3aac1d2399b82fd8253300b99d21684b192bf9aa
TayNicks <tnikel@gmail.com>
2026-03-25 22:50:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -48,6 +48,6 @@ def log_procedure():
     return redirect(url_for('dashboard'))
 # WRONG (The error you see)
 if __name__ == '__main__':
-with app.app_context():
-    db.create_all()
-app.run(host='0.0.0.0', port=10000)
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=10000)
